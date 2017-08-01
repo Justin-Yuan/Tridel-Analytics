2998c820696d346346d5a602e3495ff749545278
Zhaocong Yuan <justin.yuan@mail.utoronto.ca>
2017-07-31 22:05:44 -0400

    finished coding autoencoder

--- src/AE.py
+++ src/AE.py
@@ -12,7 +12,7 @@ from utility import get_data, next_batch
 class Autoencoder(object):
     '''
     '''
-    def __init__(self):
+    def __init__(self, path="autoencoder.csv"):
         self.learning_rate = 0.0001
         self.epochs = 800
         self.batch_size = 50
@@ -24,6 +24,14 @@ class Autoencoder(object):
         self.n_hidden_2 = 6
         self.n_hidden_3 = 1
         self.n_input = 22
+
+        self.load_data(path)
+
+    def load_data(self, path):
+        try:
+            self.mat = get_data(path)
+        except:
+            print("error when loading data")
 
     def set_epochs(self, epoch):
         self.epochs = epoch 
@@ -49,61 +57,76 @@ class Autoencoder(object):
         }
 
     def encoder(self, x):
-        layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
-        layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
-        layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['encoder_h3']), biases['encoder_b3']))
+        layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, self.weights['encoder_h1']), self.biases['encoder_b1']))
+        layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, self.weights['encoder_h2']), self.biases['encoder_b2']))
+        layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, self.weights['encoder_h3']), self.biases['encoder_b3']))
         return layer_3
 
     def decoder(self, x):
-        layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
-        layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
-        layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['decoder_h3']), biases['decoder_b3']))
+        layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, self.weights['decoder_h1']), self.biases['decoder_b1']))
+        layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, self.weights['decoder_h2']), self.biases['decoder_b2']))
+        layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, self.weights['decoder_h3']), self.biases['decoder_b3']))
         return layer_3
 
-    def build(self):
+    def build(self, path="autoencoder.csv"):
         # Construct model 
-        encoder_op = encoder(self.X)
-        decoder_op = decoder(encoder_op) 
-        y_pred = decoder_op 
-        y_true = self.X
+        self.encoder_op = encoder(self.X)
+        self.decoder_op = decoder(self.encoder_op) 
+        self.y_pred = self.decoder_op 
+        self.y_true = self.X
 
-        cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2)) + 
-        optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+        self.cost = tf.reduce_mean(tf.pow(self.y_true - self.y_pred, 2)) + 
+        self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.cost)
+        self.init = tf.global_variables_initializer()  
 
-        init = tf.global_variables_initializer()
+        self.sess = tf.Session() 
+        self.sess.run(self.init) 
+        self.saver = tf.train.Saver()   
 
-    def train(self):
+    def train(self, overwrite=False):
+        total_c = []
+        total_batch = int(self.mat.shape[0]/self.batch_size)
+        
+        if overwrite:
+            self.sess.run(self.init)
 
-        with tf.Session() as sess:
-            sess.run(init)
-            total_c = []
-            total_batch = int(mat.shape[0]/batch_size)
+        for epoch in range(self.epochs):
+            for i in range(total_batch):
+                self.index, batch_xs = next_batch(self.mat, self.index, self.batch_size)
+                _, c = self.sess.run([self.optimizer, self.cost], feed_dict={X: batch_xs})
             
-            for epoch in range(training_epochs):
-                for i in range(total_batch):
-                    index, batch_xs = next_batch(mat, index, batch_size)
-                    _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
-                
-                total_c.append(c)
-                if epoch % display_step == 0:
-                    print("Epoch:", '%04d' % (epoch+1),"cost=", "{:.9f}".format(c))
-            print("Optimization Finished!")
-            
-            score = sess.run(encoder_op, feed_dict={X: mat})
-            scores = list(score)
+            total_c.append(c)
+            if epoch % self.display_step == 0:
+                print("Epoch:", '%04d' % (epoch+1),"cost=", "{:.9f}".format(c))
+        print("Optimization Finished!")
 
-    def predict(self):
-        pass 
+    def predict(self, input_data=None):
+        if input_data == None:
+            input_data = self.mat
+        
+        score = self.sess.run(self.encoder_op, feed_dict={X: input_data)})
+        scores = list(score)
+        return scores 
 
-    def evaluate(self):
-        pass  
+    def save(self, path="model/model.ckpt"):
+        save_path = self.saver.save(self.sess, path)
+        print("model saved")
 
-    def save(self, path):
-        pass 
+    def load(self, path="model/model.ckpt", sess=None):
+        if sess == None:
+            sess = self.sess
+        self.saver.restore(sess, path)
+        print("Model restored.")
 
-    def load(self, path):
-        pass
-
+    def close_sess(self):
+        self.sess.close()
+        print("closed session")
     
 class NeighborhoodAE(Autoencoder):
-    pass 
+     
+
+
+if __name__ == "__main__":
+    """ test the autoencoders 
+    """
+
